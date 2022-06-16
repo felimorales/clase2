63e20a8b3bb9bd54eaf35d3a7d329b0d1a9cc27e
felimorales <felix.orc@hotmail.com>
2022-06-17 02:08:32 +0300

    primer clase JS

---
+++ index.js
@@ -0,0 +1,28 @@
+// comentario de linea
+
+/* alt + shift + a , para comentario en windows */
+
+//formas de declarar una variable recomendada
+
+/*let nombre = "Agostina";
+const edad = 15;*/
+
+// no recomendada
+/*var apellido = "Ruiz";
+
+console.log(nombre);
+
+console.log(typeof(edad));*/
+
+/*let numeroUno = 10;
+let numeroDos = 11;
+let resultado = numeroUno + numeroDos;
+
+console.log(resultado);*/
+
+let nombre = "andres";
+let apellido = "ruiz";
+
+let resultado = nombre + " " + apellido;
+console.log(resultado);
+
